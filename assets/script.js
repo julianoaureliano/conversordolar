1dbdc1f18c7c01e9d88fa438003c6b1afc3e45cd
JulianoAureliano <juliano.aureliano2013@gmail.com>
2025-01-22 21:31:34 -0300

    V1.0

---
+++ assets/script.js
@@ -0,0 +1,71 @@
+let dolar = 6.1;
+
+let usdInput = document.querySelector("#usd");
+let brlInput = document.querySelector("#brl");
+let botao = document.querySelector("#botao");
+
+botao.addEventListener("click", () => {
+  alert("Tem nada aqui não :) ");
+});
+usdInput.addEventListener("keyup", () => {
+  convert("usd-to-brl");
+});
+
+brlInput.addEventListener("keyup", () => {
+  convert("brl-to-usd");
+});
+//formatar os valores quando o mouse nao esta mais no foco
+usdInput.addEventListener("blur", () => {
+  usdInput.value = formatCurrency(usdInput.value);
+});
+//formatar os valores quando o mouse nao esta mais no foco
+brlInput.addEventListener("blur", () => {
+  brlInput.value = formatCurrency(brlInput.value);
+});
+
+usdInput.value = "1000,00";
+convert("usd-to-brl");
+
+function formatCurrency(value) {
+  //ajustar o valor
+  let fixedValue = fixValue(value);
+  let options = {
+    userGrouping: false,
+    minimumFractionDigits: 2,
+  };
+
+  //utilizar a faunçao formatar
+  let formatter = new Intl.NumberFormat("pt-BR", options);
+
+  //retorna valor formatado
+  return formatter.format(fixedValue);
+}
+
+function fixValue(value) {
+  let fixedValue = value.replace(",", "."); //.replace= função do javascrpit que substitue algo por outro
+  let floatValue = parseFloat(fixedValue);
+  if (floatValue == NaN) {
+    floatValue = 0;
+  }
+  return floatValue;
+}
+function convert(type) {
+  if (type == "usd-to-brl") {
+    let fixedValue = fixValue(usdInput.value);
+    let result = fixedValue * dolar;
+    result = result.toFixed(2);
+    brlInput.value = formatCurrency(result);
+    //converter o valor
+    //mostra no campo de real
+  }
+
+  if (type == "brl-to-usd") {
+    //ajustar o valor trocar a virgula pelo ponto
+    //converter o valor
+    //mostra no campo de usd
+    let fixedValue = fixValue(brlInput.value);
+    let result = fixedValue / dolar;
+    result = result.toFixed(2);
+    usdInput.value = formatCurrency(result);
+  }
+}
